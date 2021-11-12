c169f4e18a7618451941187ee9909be58997a007
schwarzlichtbezirk <schwarzlichtbezirk@gmail.com>
2021-11-12 10:28:19 +0200

    moved to go1.17.

--- go.mod
+++ go.mod
@@ -1,10 +1,18 @@
 module github.com/schwarzlichtbezirk/pds
 
-go 1.16
+go 1.17
 
 require (
 	github.com/gorilla/mux v1.8.0
-	google.golang.org/grpc v1.40.0
+	google.golang.org/grpc v1.42.0
 	google.golang.org/protobuf v1.27.1
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.0.0-20211109214657-ef0fda0de508 // indirect
+	golang.org/x/sys v0.0.0-20211110154304-99a53858aa08 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20211104193956-4c6863e31247 // indirect
 )
--- tool/build.win.x64.cmd
+++ tool/build.win.x64.cmd
@@ -1,6 +1,6 @@
 @echo off
 go env -w GOOS=windows GOARCH=amd64
-cd /d %GOPATH%/bin/
-go build -v -o pds.server.x64.exe github.com/schwarzlichtbezirk/pds/server
-go build -v -o pds.client.x64.exe github.com/schwarzlichtbezirk/pds/client
-xcopy %GOPATH%\src\github.com\schwarzlichtbezirk\pds\config pds-config /f /d /i /s /e /k /y
+cd /d %GOPATH%\src\github.com\schwarzlichtbezirk\pds
+go build -v -o %GOPATH%/bin/pds.server.x64.exe github.com/schwarzlichtbezirk/pds/server
+go build -v -o %GOPATH%/bin/pds.client.x64.exe github.com/schwarzlichtbezirk/pds/client
+xcopy .\config %GOPATH%\bin\pds-config /f /d /i /s /e /k /y
